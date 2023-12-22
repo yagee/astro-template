8d83438f8cf506aa79c8c06160c524f895d6d067
Roman Ryzhikov <general@ya.ru>
2023-12-22 23:30:33 +0200

    Add astro-compress integration to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
 
+import compress from 'astro-compress';
 import relativeLinks from 'astro-relative-links';
 import typograf from 'astro-typograf';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [typograf('p, h1, h2, h3, div, span'), relativeLinks()],
+  integrations: [
+    typograf('p, h1, h2, h3, div, span'),
+    relativeLinks(),
+    compress(),
+  ],
 });
