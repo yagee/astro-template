c6c16a2d52e6d8d770c91b6e73488300a1be00b7
Roman Ryzhikov <general@ya.ru>
2024-01-06 15:38:17 +0200

    astro-compress options

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,41 @@ import compress from 'astro-compress';
 import relativeLinks from 'astro-relative-links';
 import typograf from 'astro-typograf';
 
+const terserOptions = {
+  compress: false,
+  mangle: false,
+  output: {
+    comments: false,
+  },
+};
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     typograf('p, h1, h2, h3, div, span'),
     relativeLinks(),
-    compress(),
+    compress({
+      HTML: {
+        'html-minifier-terser': {
+          collapseBooleanAttributes: true,
+          // collapseWhitespace: true,
+          // sortAttributes: true,
+          // removeEmptyAttributes: true,
+          removeAttributeQuotes: false,
+          minifyJS: terserOptions,
+          minifyCSS: false,
+        },
+      },
+      JavaScript: terserOptions,
+      SVG: {
+        svgo: {
+          plugins: [
+            {
+              inlineStyles: false,
+            },
+          ],
+        },
+      },
+    }),
   ],
 });
