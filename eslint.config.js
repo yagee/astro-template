3ae75e3449f0f8d50da8ab7636b531769d55c174
Roman Ryzhikov <general+github@ya.ru>
2024-05-05 21:17:18 +0200

    Update ESLint configuration and dependencies

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import js from '@eslint/js';
+import eslintPluginAstro from 'eslint-plugin-astro';
+import globals from 'globals';
+
+export default [
+  js.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.es2022,
+      },
+    },
+    rules: {},
+  },
+  {
+    ignores: ['.vscode/*', 'node_modules/*', 'public/*', 'dist/*'],
+  },
+];
