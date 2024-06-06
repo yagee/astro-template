11a6b88766824849bc46904af521587ae4c81de5
Roman Ryzhikov <general@ya.ru>
2024-06-06 21:06:54 +0300

    add postcss-utopia, deps update

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,11 +1,13 @@
 module.exports = {
   plugins: [
+    require('postcss-utopia'),
     require('postcss-import'),
     require('postcss-nested'),
     require('autoprefixer'),
     require('postcss-media-minmax'),
     require('postcss-sort-media-queries'),
     require('postcss-combine-duplicated-selectors')(),
+    // TODO: Check if this works
     // require('postcss-combine-media-query'),
     require('cssnano')({
       preset: [
