4f9b633c42de40b3754d63073acab2618e55782c
Roman Ryzhikov <general+github@ya.ru>
2024-12-25 19:32:09 +0200

    update humans.txt

---
+++ src/pages/humans.txt.ts
@@ -0,0 +1,27 @@
+import type { APIRoute } from 'astro';
+
+const now = new Date();
+
+console.log(now);
+
+const robotsTxt = `
+/* TEAM */
+  Front-end: Roman Ryzhikov
+  Email: astro@yagee.ru
+  Twitter: napilsya
+  Location: Kaliningrad, Russia
+
+/* SITE */
+  Last update: ${now.toLocaleDateString('ru-RU')}
+  Standards: HTML5, CSS3, ES2021
+  Components: Astro, Postcss, Alpine.js, Utopia
+  Software: Windows, WSL, Ubuntu, Figma, VS Code, pnpm, Prettier, ESLint
+`.trim();
+
+export const GET: APIRoute = () => {
+  return new Response(robotsTxt, {
+    headers: {
+      'Content-Type': 'text/plain; charset=utf-8',
+    },
+  });
+};
